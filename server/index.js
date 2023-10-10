c033a67d0e81178c9a66e260ca20f36c6a79198e
EnzoSacinRuiz <Tu email aca>
2023-10-10 16:10:22 -0500

    DB created

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require('./src/db.js');
+const { sequelize } = require('./src/db.js'); // Use sequelize, not conn
 const PORT = 3001;
 
-conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+sequelize.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}).catch(error => console.error(error));
--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,12 +1,37 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('Driver', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING, // Assuming you store the image URL as a string
+      allowNull: false,
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   });
+
+  
 };
